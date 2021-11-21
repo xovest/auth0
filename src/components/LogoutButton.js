8ec2f90fe036603ddc1ce3cc654291acfba29616
xovest <79008917+xovest@users.noreply.github.com>
2021-11-21 22:31:01 +0200

    add the info from the user if login

--- src/components/LogoutButton.js
+++ src/components/LogoutButton.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
-const LoginButton = () => {
+const LogoutButton = () => {
   const { logout } = useAuth0();
   return (
     <button onClick={() => logout()}>
@@ -10,4 +10,4 @@ const LoginButton = () => {
   );
 }
 
-export default LoginButton;
+export default LogoutButton;
