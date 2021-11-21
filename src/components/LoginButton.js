efb1443df077fbee350728abbe263c6b40f74fac
xovest <79008917+xovest@users.noreply.github.com>
2021-11-21 22:36:55 +0200

    ez auth + loading also

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const LoginButton = () => {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
   return (
-    <button onClick={() => loginWithRedirect()}>
-      Just login already
-    </button>
+    !isAuthenticated && (
+      <button onClick={() => loginWithRedirect()}>
+        Just login already
+      </button>
+    )
   );
 }
 
