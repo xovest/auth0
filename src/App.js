367d5ba7507145efdc6e3aee1bea232249f2b911
xovest <79008917+xovest@users.noreply.github.com>
2021-11-21 22:26:02 +0200

    loggin in and out

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <LoginButton />
+      <LogoutButton />
+    </>
   );
 }
 
